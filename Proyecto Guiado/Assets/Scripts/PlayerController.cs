b300a9949926821c3d7b5a946c56b2e0dc17ff5d
Alejo Lopez <alejandro.lopezp@upb.edu.co>
2021-11-02 12:49:27 -0500

    Modificación array en PlayerController

--- Proyecto Guiado/Assets/Scripts/PlayerController.cs
+++ Proyecto Guiado/Assets/Scripts/PlayerController.cs
@@ -10,7 +10,8 @@ public class PlayerController : MonoBehaviour
     public float health = 100;
     public Text winner;
     Movement movement;
-    Gun[] guns;
+    //Gun[] guns;
+    Gun gun;
     //GameObject  player1, player2;
     public Slider healthbar;
 
@@ -28,7 +29,8 @@ public class PlayerController : MonoBehaviour
     {
 
         movement = GetComponent<Movement>();
-        guns = GetComponentsInChildren<Gun>();
+        //guns = GetComponentsInChildren<Gun>();
+        gun = GetComponentInChildren<Gun>();
     }
     void Update()
     {
@@ -36,11 +38,13 @@ public class PlayerController : MonoBehaviour
         movement.Sound();
         movement.SetLights();
         movement.Rotate();
-        for (int i = 0; i < guns.Length; i++)
+        gun.Rotate();
+        gun.Shoot();
+        /*for (int i = 0; i < guns.Length; i++)
         {
             guns[i].Rotate();
             guns[i].Shoot();
-        }      
+        } */     
     }
     public void Heal(float recover)
     {
